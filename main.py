e76b8e2315f041f443bc99ec4146a0124a9bdec4
Rafey <rafeldo36@gmail.com>
2024-01-21 15:19:01 +0530

    Operators

--- main.py
+++ main.py
@@ -30,25 +30,39 @@
 # escape sequence characters
 # print("This will \" execute",13,sep="@", end="bye")
 
-a = complex(8, 2)
-b = True
-c = "Harry"
-d = None
-print(a)
-print(b)
-a1 = 9
-print(a + a1)
-print("The type of a is ", type(a))
-print("The type of b is ", type(b))
-print("The type of c is ", type(c))
+# a = complex(8, 2)
+# b = True
+# c = "Harry"
+# d = None
+# print(a)
+# print(b)
+# a1 = 9
+# print(a + a1)
+# print("The type of a is ", type(a))
+# print("The type of b is ", type(b))
+# print("The type of c is ", type(c))
 
 
-list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
-print(list1)
+# list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+# print(list1)
 
-tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
-print(tuple1)
+# tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+# print(tuple1)
 
-dict1 = {"name":"Sakshi", "age":20, "canVote":True}
-print(dict1)
+# dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+# print(dict1)
     
+n = 15
+m = 7
+ans1 = n+m
+print("Addition of",n,"and",m,"is", ans1)
+ans2 = n-m
+print("Subtraction of",n,"and",m,"is", ans2)
+ans3 = n*m
+print("Multiplication of",n,"and",m,"is", ans3)
+ans4 = n/m
+print("Division of",n,"and",m,"is", ans4)
+ans5 = n%m
+print("Modulus of",n,"and",m,"is", ans5)
+ans6 = n//m
+print("Floor Division of",n,"and",m,"is", ans6)
