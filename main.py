ca605e7ad889d090a3917e83c1bacf48201e2cf2
Rafey <rafeldo36@gmail.com>
2024-01-21 15:48:41 +0530

    continue statement

--- main.py
+++ main.py
@@ -286,10 +286,15 @@
 #   if not number > 0:
 #     break
 
-for i in range(1,101,1):
-    print(i ,end=" ")
-    if(i==50):
-        break
-    else:
-        print("Mississippi")
-print("Thank you")
+# for i in range(1,101,1):
+#     print(i ,end=" ")
+#     if(i==50):
+#         break
+#     else:
+#         print("Mississippi")
+# print("Thank you")
+
+for i in [2,3,4,6,8,0]:
+    if (i%2!=0):
+        continue
+    print(i)
