192d5c2d15a265f84d5d879f0ff18485ad68df13
Rafey <rafeldo36@gmail.com>
2024-01-21 15:15:10 +0530

    print statements

---
+++ main.py
@@ -0,0 +1,32 @@
+# print("Hello World")
+# print(5)
+
+# import pandas
+# # Read and work with a file named 'sample.csv'
+# df = pandas.read_csv('sample.csv')
+# print(df) # This will display first few rows from the words.csv file
+
+# import hashlib  # This is an example of built in module
+# m = hashlib.sha256()
+
+# # Dont worry about how to use these modules just yet!
+
+# print("Hello World", 7)
+# print(5)
+# print("Bye")
+# print(17*13)
+
+#This is a 'Single-Line Comment'
+# print("This is a print statement.")
+
+# """This is an if-else statement.
+# It will execute a block of code if a specified condition is true.
+# If the condition is false then it will execute another block of code."""
+# p = 7
+# if (p > 5):
+#     print("p is greater than 5.")
+# else:
+#     print("p is not greater than 5.")
+# escape sequence characters
+print("This will \" execute",13,sep="@", end="bye")
+    
